c332690414e77aea7e1237879cf45bd66dd51b62
str846292 <stretton.steven@gmail.com>
2015-10-31 18:54:02 +0000

    First commit file

---
+++ BasicPhpProgramming/listmaker.php
@@ -0,0 +1,11 @@
+<?php
+
+class Listmaker{
+
+    const nonList = "bread butter ham cheese cucumber lettuce";
+    public function makeListFromSpace() {
+
+        echo("Non-list of the following: ");
+
+    }
+}
