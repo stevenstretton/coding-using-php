93c195a1d5a9de422eb31457338a922837a5bad2
str846292 <stretton.steven@gmail.com>
2015-11-01 21:04:10 +0000

    Added space removal functionality and creates list

--- BasicPhpProgramming/listmaker.php
+++ BasicPhpProgramming/listmaker.php
@@ -1,11 +1,28 @@
 <?php
 
-class Listmaker{
+        $nonList = "bread    butter    ham   cheese   cucumber lettuce\n";
+        $space = " ";
+        $newLine = "\n";
 
-    const nonList = "bread butter ham cheese cucumber lettuce";
-    public function makeListFromSpace() {
+        echo "Items for spaces to be removed and listed: ", $nonList, "\n";
 
-        echo("Non-list of the following: ");
+        $removeMultiSpace = preg_replace('/\s+/',' ', $nonList);
+        echo "\n****************************\nMulitple spaces removed\nCreating List\n****************************\n\n\n";
+        echo $removeMultiSpace, "\n\n\n";
+        $createList = str_replace($space,$newLine,$removeMultiSpace);
+        echo "\n****************************\nList Completed\nDisplaying Results\n****************************\n";
+        echo $createList;
+        echo "\n****************************\nProcess Complete\n****************************\n";
 
-    }
-}
+
+//class listmaker {
+//
+//
+//        public $nonList = 'bread butter ham cheese cucumber lettuce';
+//        public $space = " ";
+//        public $newLine = "\n";
+//
+//        $createList = str_replace($space,$newLine,$nonList);
+//        echo $this->$createList;
+//
+//}
